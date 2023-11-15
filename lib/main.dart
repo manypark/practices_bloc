03efdda89f7e53423018c9097346736d186d015a
Manu <manypark@live.com>
2023-11-15 15:04:14 -0600

    feat: firebase core install

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:practices/config/config.dart';
-import 'package:practices/features/products/presentation/bloc/blocs.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:practices/firebase_options.dart';
 
+import 'package:practices/config/config.dart';
 import 'features/login/presentation/bloc/login_form_bloc.dart';
+import 'package:practices/features/products/presentation/bloc/blocs.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   serviceLocatorInit();
   runApp(const BlocsProviders());
 }
